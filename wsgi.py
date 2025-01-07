76468f3681ea529d8ef8d2506cd7561717516fb6
Torekas <janw-1990@wp.pl>
2025-01-07 22:17:13 +0100

    Updating wsgi module

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 # wsgi.py
 
-from app import app
+from app import app as application
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host='0.0.0.0', port=8080)
+    serve(application, host='0.0.0.0', port=8080)
